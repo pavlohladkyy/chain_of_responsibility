7ce252760113ed4be5685929fc18e3208efbee31
Павло Гладкий <hladkyy.p@nltu.lviv.ua>
2024-11-14 16:28:35 +0200

    створюємо дочірній класс обробник для банківського переказу,для переказу по western-uninon,для переказу по Pay Pal,створення класу для системи переказів

--- lab7.py
+++ lab7.py
@@ -9,4 +9,44 @@ class transfer_handler:
             self.next_handler.handle(transfer)
 
 
+#створюємо дочірній класс обробник для банківського переказу
+class bank_transfer_handler(transfer_handler):
+    def handle (self,transfer):
+        if transfer['metod']=='bank':
+              print(f"Переказ через банківський переказ на суму: {transfer['amount']}")
+
+        else:
+             super().handle(transfer)
+
+#cтворюємо дочірній клас обробник для переказу по western-uninon 
+class western_union_transter_handle(transfer_handler):
+    def handle (self,transfer):
+        if transfer['metod']=='westerUnion':
+              print(f"Переказ через Western Union на суму: {transfer['amount']}")
+
+        else:
+             super().handle(transfer)
+
+#створюємо дочірній клас обробник для переказу по Pay Pal
+class pay_pal_transter_handle(transfer_handler):
+    def handle (self,transfer):
+        if transfer['metod']=='payPal':
+              print(f"Переказ через Western Union на суму: {transfer['amount']}")
+
+        else:
+             super().handle(transfer)
+
+#створення класу для системи переказів
+class Transfer_System:
+    def __init__(self):
+        #ланцюгпередання 
+        self.handler_chain= bank_transfer_handler(
+            western_union_transter_handle(pay_pal_transter_handle())
+        )
+
+    def make_transfer(self,amount,method):
+        transfer = {'amount':amount,'method':method}
+        # Починаємо обробку переказу через ланцюг
+        self.handler_chain.handle(transfer)
+
 
