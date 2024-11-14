26e69224d56726e01e52a9d0a7d46ec67ca597ab
Павло Гладкий <hladkyy.p@nltu.lviv.ua>
2024-11-14 16:52:16 +0200

    створені методи для керування ланцюгом а саме вводу суми та вибору системи переказу

--- lab7.py
+++ lab7.py
@@ -13,7 +13,7 @@ class transfer_handler:
 class bank_transfer_handler(transfer_handler):
     def handle (self,transfer):
         if transfer['metod']=='bank':
-              print(f"Переказ через банківський переказ на суму: {transfer['amount']}")
+              print(f"успішно! виконано переказ через банківський переказ на суму: {transfer['amount']}")
 
         else:
              super().handle(transfer)
@@ -21,8 +21,8 @@ class bank_transfer_handler(transfer_handler):
 #cтворюємо дочірній клас обробник для переказу по western-uninon 
 class western_union_transter_handle(transfer_handler):
     def handle (self,transfer):
-        if transfer['metod']=='westerUnion':
-              print(f"Переказ через Western Union на суму: {transfer['amount']}")
+        if transfer['metod']=='Wester Union':
+              print(f"успішно! виконано переказ через Western Union на суму: {transfer['amount']}")
 
         else:
              super().handle(transfer)
@@ -30,8 +30,8 @@ class western_union_transter_handle(transfer_handler):
 #створюємо дочірній клас обробник для переказу по Pay Pal
 class pay_pal_transter_handle(transfer_handler):
     def handle (self,transfer):
-        if transfer['metod']=='payPal':
-              print(f"Переказ через Western Union на суму: {transfer['amount']}")
+        if transfer['metod']=='Pay Pal':
+              print(f"успішно! виконано переказ через Western Union на суму: {transfer['amount']}")
 
         else:
              super().handle(transfer)
@@ -45,8 +45,43 @@ class Transfer_System:
         )
 
     def make_transfer(self,amount,method):
-        transfer = {'amount':amount,'method':method}
+        transfer = {'amount':amount,'metod':method}
         # Починаємо обробку переказу через ланцюг
         self.handler_chain.handle(transfer)
 
+def enter_sum():
+    sum = int(input("введіть суму: "))
+    return sum
 
+
+def enter_method():
+    print("1.Pay Pal")
+    print("2.Wester Union")
+    print("3.bank")
+    print("4.Exit")
+    method = int(input("введіть номер системи яку бажаєте викристати: "))
+    transfer_system = Transfer_System()
+
+    if method == 1:
+        sum = enter_sum()
+        transfer_system.make_transfer(sum, 'Pay Pal')
+    elif method == 2:
+        sum = enter_sum()
+        transfer_system.make_transfer(sum, 'Wester Union')
+    elif method == 3:
+        sum = enter_sum()
+        transfer_system.make_transfer(sum, 'bank')
+    elif method == 4:
+        print("Exit")
+        exit()
+    else:
+        print("wrong choice")
+
+
+def main():
+    card_number = int(input("введіть номер карти отримувача: "))
+    enter_method()
+
+
+if __name__ == "__main__":
+    main()
